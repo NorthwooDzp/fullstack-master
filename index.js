1008489dee819e5acd357b5b1a4c96c3cb4f9f81
NorthwooDzp <northwood@list.ru>
2018-07-06 23:06:32 +0300

    B2 L3 start

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-const express = require('express');
+const app = require('./app');
+const port = process.env.PORT || 5000;
 
-const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'working'
-    });
-});
-
-app.listen(5000, () => {
-    console.log('server has been started');
+app.listen(port, () => {
+    console.log(`Server has been started on port ${port}`);
 });
