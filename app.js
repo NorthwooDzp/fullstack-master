1b6c67c494076a972ae5416b2996e5968f111176
NorthwooDzp <northwood@list.ru>
2018-07-24 22:44:54 +0300

    B2 L5 all routes created

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
+
 const authRoutes = require('./routes/auth');
+const orderRoutes = require('./routes/order');
+const categoryRoutes = require('./routes/category');
+const analyticsRoutes = require('./routes/analytics');
+const positionRoutes = require('./routes/position');
 
 app.use(express.json());
+
 app.use('/api/auth', authRoutes);
+app.use('/api/order', orderRoutes);
+app.use('/api/analytics', analyticsRoutes);
+app.use('/api/category', categoryRoutes);
+app.use('/api/position', positionRoutes);
 
 module.exports = app;
