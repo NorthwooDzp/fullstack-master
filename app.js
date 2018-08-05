40f57dd792f9ce59ee8a146e9d4404f593ca413b
NorthwooDzp <northwood@list.ru>
2018-08-05 22:15:22 +0300

    B2 L7 additional tools cors and morgan added

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const cors = require('cors');
-const morgan = require('morgan');
 
 const authRoutes = require('./routes/auth');
 const orderRoutes = require('./routes/order');
@@ -10,10 +8,10 @@ const categoryRoutes = require('./routes/category');
 const analyticsRoutes = require('./routes/analytics');
 const positionRoutes = require('./routes/position');
 
-app.use(morgan('dev'));
+app.use(require('morgan')('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors());
+app.use(require('cors')());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/order', orderRoutes);
