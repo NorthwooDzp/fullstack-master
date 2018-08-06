24a20b5fd40339750aa7437d6629d50235f05f60
NorthwooDzp <northwood@list.ru>
2018-08-07 01:12:11 +0300

    B2 L9 DB connected

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://testCRMAdmin:testCRM@127.0.0.1:27017/testCRM'
+}
+
+/*db.createUser({user: "testCRMAdmin", pwd: "testCRM", roles: [{role: "dbOwner", db: "testCRM"}, {role: "dbAdmin", db: "testCRM"}, {role: "userAdmin", db: "testCRM"}, {role: "readWrite", db: "testCRM"}]})*/
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const app = require('./app');
 const port = process.env.PORT || 5000;
+const db = require('./db/db');
+const config = require('./config/keys');
 
-app.listen(port, () => {
-    console.log(`Server has been started on port ${port}`);
-});
+db.connect(config.mongoURI)
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server has been started on port ${port}`);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
