e17cbe88e45adda9d71080b809cbcd1a850ff762
NorthwooDzp <northwood@list.ru>
2018-08-17 16:01:30 +0300

    B4 L4 file uploading configuration done

--- middleware/upload.js
+++ middleware/upload.js
@@ -3,20 +3,26 @@ const moment = require('moment');
 
 const storage = multer.diskStorage({
     destination(req, file, callback) {
-        callback(null, 'uploads/')
+        callback(null, 'uploads/');
     },
     filename(req, file, callback) {
         const date = moment().format('DD_MM_YYYY-HHmmss_SSS');
-        callback(null, `${file.originalname}-${date}`)
+        callback(null, `${file.originalname}-${date}`);
     }
 });
 
 const fileFilter = (req, file, callback) => {
-    if (file.mimetype === 'image/*') {
-        console.log(file);
+    if (file.mimetype === 'image/png' || file.mimetype == 'image/jpeg') {
+        callback(null, true);
+    } else {
+        cb(null, false);
     }
 };
 
+const limits = {
+    filesize: 1024 * 1024 * 5
+};
+
 module.exports = multer({
-    storage
+    storage, fileFilter, limits
 });
